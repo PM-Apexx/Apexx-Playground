7b44f33474a852afe89728f1c1ce1274f4b125e5
PM-Apexx <125660663+PM-Apexx@users.noreply.github.com>
2024-04-26 16:01:51 +0100

    Update payment.js

--- Payment-demo/payment.js
+++ Payment-demo/payment.js
@@ -513,7 +513,7 @@ const initiateSezzlePayment = async () => {
       capture_now: true,
       dynamic_descriptor: 'Apexx Test',
       merchant_reference: 'jL9ZJMjoYIuFIrH,
-      return_url: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-response.html',
+      return_url: 'https://sandbox.apexx.global/atomic/v1/api/return',
       webhook_transaction_update: 'https://webhook.site/63250144-1263-4a3e-a073-1707374c5296',
       transaction_type: 'first',
       duplicate_check: false,
