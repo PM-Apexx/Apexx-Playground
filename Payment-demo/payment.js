acea38d288d99196d8170d7ef8903b0ee2d63104
PM-Apexx <125660663+PM-Apexx@users.noreply.github.com>
2024-04-29 15:24:29 +0100

    Update payment.js

--- Payment-demo/payment.js
+++ Payment-demo/payment.js
@@ -192,7 +192,7 @@ const initiateKlarnaPayment = async () => {
       ]
     },
     redirect_urls: {
-      success: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-response.html',
+      success: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-response.html?status=Success',
       failed: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-response.html?status=failed',
       cancelled: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-response.html?status=cancelled'
     },
