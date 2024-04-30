6b69bf09a6c85ace59f22c5aa120aeb77f5dc67f
PM-Apexx <125660663+PM-Apexx@users.noreply.github.com>
2024-04-30 15:21:44 +0100

    Update payment.js

--- Payment-demo/payment.js
+++ Payment-demo/payment.js
@@ -450,6 +450,7 @@ const initiateZipPayment = async () => {
     try {
       const responseData = await apiClient.sendRequest('', 'POST', paymentData, 'hosted');
       if (responseData && responseData.url) {
+        window.location.href = responseData.url;
         const paymentIframe = document.getElementById('payment-iframe');
         if (paymentIframe) {
           paymentIframe.onload = () => {
