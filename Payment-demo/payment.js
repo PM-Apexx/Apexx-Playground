93f33e3ff4284f4e7936cb12f01d150875c3c186
PM-Apexx <125660663+PM-Apexx@users.noreply.github.com>
2024-05-08 12:33:57 +0100

    Update payment.js

--- Payment-demo/payment.js
+++ Payment-demo/payment.js
@@ -445,9 +445,7 @@ const initiateZipPayment = async () => {
         three_ds_version: '2.0'
        },
       show_custom_fields: {
-        card_holder_name: "true",
-        address: "true",
-        address_required: "true"
+        card_holder_name: "true"
       },
       show_order_summary: "false",
       transaction_css_template: `  /* Frame Style - Start */
