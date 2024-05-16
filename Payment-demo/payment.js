f09d824c63288df9de4e1baeb55e1296e322b2ab
PM-Apexx <125660663+PM-Apexx@users.noreply.github.com>
2024-05-16 10:10:57 +0100

    Update payment.js

--- Payment-demo/payment.js
+++ Payment-demo/payment.js
@@ -43,6 +43,24 @@ class ApiClient {
   }
 }
 
+function handlePaymentResponse() {
+  // Get the productUrl query parameter from the URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const productUrl = urlParams.get('productUrl') || 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/index.html';
+
+  // Redirect back to the products page
+  const productsSection = document.querySelector('.products');
+  if (productsSection) {
+    productsSection.style.display = 'flex';
+  } else {
+    window.location.href = productUrl;
+  }
+
+  // Clear the basket and update the basket count after successful payment
+  basket = [];
+  updateBasketCount();
+}
+
 const items = [
   {
     product_id: "12345",
@@ -85,7 +103,6 @@ const updateBasketCount = () => {
   const cartButton = document.getElementById('cart');
   cartButton.textContent = `Basket (${basket.length})`;
 };
-
 const paymentMethodRadios = document.querySelectorAll('input[name="payment-method"]');
 
 paymentMethodRadios.forEach(radio => {
@@ -123,6 +140,7 @@ alternativeMethodLogos.forEach(logo => {
     }
   });
 });
+
 function handlePaymentMethodChange() {
   const alternativeMethodsDiv = document.getElementById('alternative-methods');
   const selectedMethod = document.querySelector('input[name="payment-method"]:checked').value;
@@ -135,6 +153,7 @@ function handlePaymentMethodChange() {
     selectedAlternativeMethod = null;
   }
 }
+
 const displayPaymentForm = () => {
   const paymentForm = document.getElementById('payment-form');
   if (paymentForm) {
@@ -169,11 +188,11 @@ const initiateKlarnaPayment = async () => {
       ]
     },
     redirect_urls: {
-      success: 'payment-success.html',
-      failed: 'payment-success.html',
-      cancelled: 'payment-success.html'
+      success: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html?returnUrl=https://pm-apexx.github.io/Apexx-Playground/Payment-demo/index.html',
+      failed: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html',
+      cancelled: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html'
     },
-      items: items,
+    items: items,
     customer: {
       customer_identification_number: 'string',
       identification_type: 'SSN',
@@ -227,7 +246,6 @@ const initiateKlarnaPayment = async () => {
     showError('Error initiating Klarna payment. Please try again.');
   }
 };
-
 const initiateClearpayPayment = async () => {
   const totalAmount = items.reduce((total, item) => total + item.net_unit_price, 0);
   const paymentData = {
@@ -254,9 +272,9 @@ const initiateClearpayPayment = async () => {
       ]
     },
     redirect_urls: {
-      success: 'payment-success.html',
-      failed: 'payment-success.html',
-      cancelled: 'payment-success.html'
+      success: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html?returnUrl=https://pm-apexx.github.io/Apexx-Playground/Payment-demo/index.html',
+      failed: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html',
+      cancelled: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html'
     },
     items: items,
     customer: {
@@ -312,7 +330,6 @@ const initiateClearpayPayment = async () => {
     showError('Error initiating Clearpay payment. Please try again.');
   }
 };
-
 const initiateZipPayment = async () => {
   const totalAmount = items.reduce((total, item) => total + item.net_unit_price, 0);
   const paymentData = {
@@ -339,9 +356,9 @@ const initiateZipPayment = async () => {
       ]
     },
     redirect_urls: {
-      success: 'payment-success.html',
-      failed: 'payment-success.html',
-      cancelled: 'payment-success.html'
+      success: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html?returnUrl=https://pm-apexx.github.io/Apexx-Playground/Payment-demo/index.html',
+      failed: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html',
+      cancelled: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html'
     },
     items: items,
     customer: {
@@ -384,7 +401,6 @@ const initiateZipPayment = async () => {
       method: 'delivery'
     }
   };
-
   try {
     const responseData = await apiClient.sendRequest('', 'POST', paymentData, 'bnpl');
     if (responseData && responseData.url) {
@@ -396,8 +412,8 @@ const initiateZipPayment = async () => {
     console.error('ZIP payment initiation failed:', error);
     showError('Error initiating ZIP payment. Please try again.');
   }
-};
- const initiateCardPayment = async (basket) => {
+};        
+  const initiateCardPayment = async (basket) => {
   if (!paymentInitiated) {
     //const totalAmount = basket.reduce((total, item) => total + parseInt(item.amount), 0);
     const paymentData = {
@@ -407,7 +423,7 @@ const initiateZipPayment = async () => {
       capture_now: true,
       dynamic_descriptor: 'Demo Merchant Test Purchase',
       merchant_reference: 'jL9ZJMjoYIuFIrH',
-      return_url: 'payment-success.html',
+      return_url: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html?returnUrl=https://pm-apexx.github.io/Apexx-Playground/Payment-demo/index.html',
       webhook_transaction_update: 'https://webhook.site/63250144-1263-4a3e-a073-1707374c5296',
       transaction_type: 'first',
       duplicate_check: 'false',
@@ -515,8 +531,6 @@ const initiateZipPayment = async () => {
           paymentForm.style.display = 'block';
         }
         paymentInitiated = true;
-
-        // Clear the basket and update the basket count after successful payment
         basket = [];
         updateBasketCount();
       } else {
@@ -537,18 +551,18 @@ const initiateSofortPayment = async (basket) => {
     capture_now: 'true',
     customer_ip: '10.20.0.186',
     recurring_type: 'first',
-    amount: totalAmount.toString(), // This should be dynamic based on the basket contents
+    amount: totalAmount.toString(),
     currency: 'EUR',
     user_agent: 'string',
     locale: 'en',
     dynamic_descriptor: 'Apexx SOFORT Test',
-    merchant_reference: 'jL9ZJMjoYIuFIrH', // Dynamically generate a reference
+    merchant_reference: 'CT3455640', 
     webhook_transaction_update: 'https://webhook.site/db694c36-9e0b-4c45-bbd8-596ea98fe358',
     shopper_interaction: 'ecommerce',
     sofort: {
       account_holder_name: 'Test Name',
       redirection_parameters: {
-        return_url: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html'
+        return_url: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html?returnUrl=https://pm-apexx.github.io/Apexx-Playground/Payment-demo/index.html'
       } 
     },
     customer: {
@@ -573,10 +587,10 @@ const initiateSofortPayment = async (basket) => {
       }
     }
   };
-   try {
+  try {
     const responseData = await apiClient.sendRequest('', 'POST', paymentData, 'hosted');
     if (responseData && responseData.url) {
-      window.location.href = responseData.url;
+      window.location.href = payment-response.html;
     } else {
       showError('Failed to initiate SOFORT payment');
     }
@@ -585,6 +599,17 @@ const initiateSofortPayment = async (basket) => {
     showError('Error initiating SOFORT payment. Please try again.');
   }
 };
+
+const showError = (message) => {
+  const errorElement = document.getElementById('error-message');
+  if (errorElement) {
+    errorElement.textContent = message;
+    errorElement.style.display = 'block';
+  } else {
+    alert(message);
+  }
+};
+
 const initiateBancontactPayment = async (basket) => {
   const totalAmount = basket.reduce((total, item) => total + parseInt(item.amount), 0);
   const paymentData = {
@@ -592,19 +617,19 @@ const initiateBancontactPayment = async (basket) => {
     capture_now: 'true',
     customer_ip: '10.20.0.186',
     recurring_type: 'first',
-    amount: totalAmount.toString(),
+    amount: totalAmount.toString(), 
     currency: 'EUR',
     user_agent: 'string',
     locale: 'en',
     dynamic_descriptor: 'Apexx SOFORT Test',
-    merchant_reference: 'jL9ZJMjoYIuFIrH',
+    merchant_reference: 'CT34540', 
     webhook_transaction_update: 'https://webhook.site/db694c36-9e0b-4c45-bbd8-596ea98fe358',
     shopper_interaction: 'ecommerce',
     bancontact: {
       account_holder_name: 'Test Name',
       redirection_parameters: {
-        return_url: 'payment-success.html'
-      }
+        return_url: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-response.html?returnUrl=https://pm-apexx.github.io/Apexx-Playground/Payment-demo/index2.html'
+      } 
     },
     customer: {
       first_name: 'AP',
@@ -640,11 +665,10 @@ try {
     showError('Error initiating Bancontact payment. Please try again.');
   }
 };
-
 const initiateidealPayment = async (basket) => {
-  const totalAmount = basket.reduce((total, item) => total + parseInt(item.amount), 0);
-  const paymentData = {
-    organisation: 'ff439f6eAc78dA4667Ab05aAc89f92e27f76',
+const totalAmount = basket.reduce((total, item) => total + parseInt(item.amount), 0);
+const paymentData = {
+organisation: 'ff439f6eAc78dA4667Ab05aAc89f92e27f76',
     capture_now: 'true',
     customer_ip: '10.20.0.186',
     recurring_type: 'first',
@@ -653,14 +677,14 @@ const initiateidealPayment = async (basket) => {
     user_agent: 'string',
     locale: 'en',
     dynamic_descriptor: 'Apexx ideal Test',
-    merchant_reference: 'jL9ZJMjoYIuFIrH',
+    merchant_reference: 'CT34540', // Dynamically generate a reference
     webhook_transaction_update: 'https://webhook.site/db694c36-9e0b-4c45-bbd8-596ea98fe358',
     shopper_interaction: 'ecommerce',
     ideal: {
       account_holder_name: 'Test Name',
       redirection_parameters: {
-        return_url: 'payment-success.html'
-      }
+        return_url: 'https://pm-apexx.github.io/Apexx-Playground/Payment-demo/payment-success.html?returnUrl=https://pm-apexx.github.io/Apexx-Playground/Payment-demo/index.html'
+      } 
     },
     customer: {
       first_name: 'AP',
@@ -684,7 +708,7 @@ const initiateidealPayment = async (basket) => {
       }
     }
   };
- try {
+try {
     const responseData = await apiClient.sendRequest('', 'POST', paymentData);
     if (responseData && responseData.url) {
       window.location.href = responseData.url;
@@ -696,22 +720,12 @@ const initiateidealPayment = async (basket) => {
     showError('Error initiating iDEAL payment. Please try again.');
   }
 };
-
-const showError = (message) => {
-  const errorElement = document.getElementById('error-message');
-  if (errorElement) {
-    errorElement.textContent = message;
-    errorElement.style.display = 'block';
-  } else {
-    alert(message);
-  }
-};
-
-document.addEventListener('DOMContentLoaded', () => {
+ document.addEventListener('DOMContentLoaded', () => {
   const basketButton = document.getElementById('cart');
   const backButton = document.getElementById('back-to-products');
   const productsSection = document.querySelector('.products');
   const paymentOptionsSection = document.getElementById('payment-options-page');
+  const paymentForm = document.getElementById('payment-form');
   if (paymentOptionsSection) {
     paymentOptionsSection.style.display = 'none';
   }
@@ -735,56 +749,53 @@ document.addEventListener('DOMContentLoaded', () => {
         paymentOptionsSection.style.display = 'none';
       }
       productsSection.style.display = 'flex';
+    paymentForm.style.display = 'none';
     });
   }
 
-  document.getElementById('confirm-payment').addEventListener('click', async () => {
-    const selectedMethodRadio = document.querySelector('input[name="payment-method"]:checked');
-    if (selectedMethodRadio) {
-      const selectedMethod = selectedMethodRadio.value;
-      switch (selectedMethod) {
-        case 'card':
-          await initiateCardPayment(basket);
-          break;
-        case 'alternative':
-          const selectedAlternativeMethod = document.querySelector('#alternative-methods img.selected');
-          if (selectedAlternativeMethod) {
-            const methodName = selectedAlternativeMethod.alt.toLowerCase();
-            switch (methodName) {
-              case 'ideal':
-                await initiateidealPayment(basket);
-                break;
-              case 'sofort':
-                await initiateSofortPayment(basket);
-                break;
-              case 'klarna':
-                await initiateKlarnaPayment();
-                break;
-              case 'bancontact':
-                await initiateBancontactPayment(basket);
-                break;
+document.getElementById('confirm-payment').addEventListener('click', async () => {
+  const selectedMethodRadio = document.querySelector('input[name="payment-method"]:checked');
+  if (selectedMethodRadio) {
+    const selectedMethod = selectedMethodRadio.value;
+    switch (selectedMethod) {
+      case 'card':
+        await initiateCardPayment(basket);
+        break;
+      case 'alternative':
+        const selectedAlternativeMethod = document.querySelector('#alternative-methods img.selected');
+        if (selectedAlternativeMethod) {
+          const methodName = selectedAlternativeMethod.alt.toLowerCase(); 
+          switch (methodName) {
+            case 'ideal':
+              await initiateidealPayment(basket);
+              break;
+            case 'sofort':
+              await initiateSofortPayment(basket);
+              break;
+            case 'klarna':
+              await initiateKlarnaPayment();
+              break;
+            case 'bancontact':
+              await initiateBancontactPayment(basket);
+              break;
               case 'clearpay':
-                await initiateClearpayPayment();
-                break;
-              case 'zip':
-                await initiateZipPayment();
-                break;
-              default:
-                console.error('Invalid alternative payment method selected');
-            }
-          } else {
-            console.error('No alternative payment method selected');
+              await initiateClearpayPayment(basket);
+              break;
+            default:
+              console.error('Invalid alternative payment method selected');
           }
-          break;
-        default:
-          console.error('Invalid payment method selected');
-      }
-    } else {
-      console.error('No payment method selected');
+        } else {
+          console.error('No alternative payment method selected');
+        }
+        break;
+      default:
+        console.error('Invalid payment method selected');
     }
-  });
-
-  document.querySelectorAll('.add-to-basket').forEach(button => {
+  } else {
+    console.error('No payment method selected');
+  }
+});
+document.querySelectorAll('.add-to-basket').forEach(button => {
     button.addEventListener('click', function() {
       const product = {
         name: this.getAttribute('data-name'),
@@ -793,5 +804,4 @@ document.addEventListener('DOMContentLoaded', () => {
       basket.push(product);
       updateBasketCount();
     });
-  });
-});
+  }); 
