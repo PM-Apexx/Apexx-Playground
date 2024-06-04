381db07c9d70d04866fe8a49741d3f612e91cd81
PM-Apexx <125660663+PM-Apexx@users.noreply.github.com>
2024-06-04 17:44:34 +0100

    Update payment.js

--- Payment-demo/payment.js
+++ Payment-demo/payment.js
@@ -46,7 +46,7 @@ class ApiClient {
 function handlePaymentResponse() {
   // Get the productUrl query parameter from the URL
   const urlParams = new URLSearchParams(window.location.search);
-  const productUrl = urlParams.get('productUrl') || 'https://incredible-blancmange-139f6c.netlify.app/Payment-demo/index.html';
+  const productUrl = urlParams.get('productUrl') || 'https://incredible-blancmange-139f6c.netlify.app';
 
   // Redirect back to the products page
   const productsSection = document.querySelector('.products');
